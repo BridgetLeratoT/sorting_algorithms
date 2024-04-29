d573cc72ca71ddf2447d00eb6e8f4bf39dd6f041
BridgetLeratoT <leratotshabalala146@gmail.com>
2024-04-29 10:16:40 -0700

    algorithms

---
+++ 0-bubble_sort.c
@@ -0,0 +1,34 @@
+#include "sort.h"
+
+void swap_ints(int *a, int *b)
+{
+	int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+void bubble_sort(int *array, size_t size)
+{
+	size_t l, len = size;
+	bool bubbly = false;
+
+	if (array == NULL || size < 2)
+		return;
+
+	while (bubbly == false)
+	{
+		bubbly = true;
+		for (l = 0; l < len - 1; l++)
+		{
+			if (array[l] > array[l + 1])
+			{
+				swap_ints(array + l, array + l + 1);
+				print_array(array, size);
+				bubbly = false;
+			}
+		}
+		len--;
+	}
+}
